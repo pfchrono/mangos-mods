a10db5e7609f4eb0e678682f6585db12bd416eef
pfchrono <pfchrono@gmail.com>
2009-06-22 23:12:44 -0500

    [+] Applying AHBot to master (Naicisum@getmanogs.com http://getmangos.com/community/showthread.php?t=8386) [+] Adding patches contrib [+] Added ahbot.diff from Naicisum's master diff
    Signed-off-by: pfchrono <pfchrono@gmail.com>

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -62,6 +62,7 @@
 #include "WaypointManager.h"
 #include "GMTicketMgr.h"
 #include "Util.h"
+#include "AuctionHouseBot.h"
 
 INSTANTIATE_SINGLETON_1( World );
 
@@ -1428,6 +1429,9 @@ void World::SetInitialWorldSettings()
     uint32 nextGameEvent = gameeventmgr.Initialize();
     m_timers[WUPDATE_EVENTS].SetInterval(nextGameEvent);    //depend on next event
 
+    sLog.outString("Initialize AuctionHouseBot...");
+    auctionbot.Initialize();
+
     sLog.outString( "WORLD: World initialized" );
 }
 
@@ -1497,6 +1501,7 @@ void World::Update(uint32 diff)
     /// <ul><li> Handle auctions when the timer has passed
     if (m_timers[WUPDATE_AUCTIONS].Passed())
     {
+        auctionbot.Update();
         m_timers[WUPDATE_AUCTIONS].Reset();
 
         ///- Update mails (return old mails with item, or delete them)
