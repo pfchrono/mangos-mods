2a98e21ba4e2a96c56c7c98ce2652bb95fa27c50
pfchrono <pfchrono@gmail.com>
2009-06-29 17:13:47 -0500

    [+] Importing TrinityCore2 Rv 4325
    Signed-off-by: pfchrono <pfchrono@gmail.com>

---
+++ src/shared/revision.h
@@ -0,0 +1,7 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define _REVISION "4249"
+ #define _HASH "ad84dc165979"
+ #define _REVISION_DATE "*"
+ #define _REVISION_TIME "*"
+#endif // __REVISION_H__
