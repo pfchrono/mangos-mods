00d86a9d197549ee71d23b3f9d33a439b433ad3e
pfchrono <pfchrono@gmail.com>
2009-08-17 01:00:28 -0500

    [+] Forgot to commit this for the last push for Scarlet Courier
    Signed-off-by: pfchrono <pfchrono@gmail.com>

--- sql/rs-updates/rs-70_world_big-update-dual-also.sql
+++ sql/rs-updates/rs-70_world_big-update-dual-also.sql
@@ -268,3 +268,5 @@ UPDATE `script_texts` SET `type` = '3' WHERE `entry` IN ('-1532089','-1532090');
 
 UPDATE `npc_option` SET `id`=52,`action`=19 WHERE `id`=51 AND `action`=18 AND `option_text`='UNIT_NPC_FLAG_OUTDOORPVP';
 INSERT INTO `npc_option` (`id`, `gossip_id`, `npcflag`, `icon`, `action`, `box_money`, `coded`, `option_text`, `box_text`) values('51','0','16','2','18','100000','0','Purchase a Dual Talent Specialization.','Are you sure you wish to purchase a Dual Talent Specialization?');
+
+UPDATE `creature_template` SET `ScriptName`='mob_scarlet_courier' WHERE `entry`='29076';
