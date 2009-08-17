b880c30a005aef06eb929b29ca771359223a65ca
pfchrono <pfchrono@gmail.com>
2009-08-17 07:34:05 -0500

    [+] Paladin Holy Shock and Death Knight Heart Strike removed from character_spell for Dual Talents [+] Seems revision.h is working again roflolwtf
    Signed-off-by: pfchrono <pfchrono@gmail.com>

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,7 +1,7 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "*"
+ #define _REVISION "f0d9c6b715e699f64d61600f3c353d7eca691c2b"
  #define _HASH "*"
- #define _REVISION_DATE "*"
- #define _REVISION_TIME "*"
+ #define _REVISION_DATE "2009-08-17"
+ #define _REVISION_TIME "00:53:36"
 #endif // __REVISION_H__
