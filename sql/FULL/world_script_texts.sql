4cff9e72a1cd7013fb8ea9dfd9c9d4ef60c51ceb
pfchrono <pfchrono@gmail.com>
2009-08-20 06:50:32 -0500

    [RS111] Adding query to update DB to current TC2 [TC] * Set vehicle owner guid and faction in summonvehicle instead of spelleffecthandler. [TC] * Dismiss summoned vehicle when it is dead. [TC] * Wintergrasp: only allow a workshop to build 4 vehicles. [TC] * remove all existing vehicles when workshop is destroyed or captured. [+] Fix typo in world_script_texts.sql from 5216.
    Signed-off-by: pfchrono <pfchrono@gmail.com>

--- sql/FULL/world_script_texts.sql
+++ sql/FULL/world_script_texts.sql
@@ -1,4 +1,4 @@
-﻿-- Up to TC2 4748
+-- Up to TC2 4748
 -- 
 -- trinityscript_script_texts.sql Contains data for table `script_texts` mainly used in C++ parts.
 -- valid entries for table are between -1000000 and -1999999
@@ -358,10 +358,36 @@ INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content
 (-1000413,'I apologize for taking so long to get here. I wanted Lady Proudmoore to be present also.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 7, 0,'hendel SAY_PROGRESS_3_TER'),
 (-1000414,'We can only stay a few moments before returning to the tower. If you wish to speak to us more you may find us there.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 7, 0,'hendel SAY_PROGRESS_4_TER'),
 (-1000415,'%s, too injured, gives up the chase.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0,'hendel EMOTE_SURRENDER'),
-(-1000416,'REUSE', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'REUSE'),
-(-1000417,'REUSE', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'REUSE'),
-(-1000418,'REUSE', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'REUSE'),
-(-1000419,'REUSE', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'REUSE');
+(-1000416,'Well, I\'m not sure how far I\'ll make it in this state... I\'m feeling kind of faint...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_RIN_START_1'),
+(-1000417,'Remember, if I faint again, the water that Spraggle gave you will revive me.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_RIN_START_2'),
+(-1000418,'The heat... I can\'t take it...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_FAINT_1'),
+(-1000419,'Maybe... you could carry me?', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_FAINT_2'),
+(-1000420,'Uuuuuuggggghhhhh....', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_FAINT_3'),
+(-1000421,'I\'m not feeling so well...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_FAINT_4'),
+(-1000422,'Where... Where am I?', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_WAKE_1'),
+(-1000423,'I am feeling a little better now, thank you.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_WAKE_2'),
+(-1000424,'Yes, I must go on.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_WAKE_3'),
+(-1000425,'How am I feeling? Quite soaked, thank you.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_WAKE_4'),
+(-1000426,'Spraggle! I didn\'t think I\'d make it back!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_RIN_END_1'),
+(-1000427,'Ringo! You\'re okay!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_SPR_END_2'),
+(-1000428,'Oh... I\'m feeling faint...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_RIN_END_3'),
+(-1000429,'%s collapses onto the ground.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,2,0,0,'ringo EMOTE_RIN_END_4'),
+(-1000430,'%s stands up after a short pause.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,2,0,0,'ringo EMOTE_RIN_END_5'),
+(-1000431,'Ugh.',NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_RIN_END_6'),
+(-1000432,'Ringo? Wake up! Don\'t worry, I\'ll take care of you.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'ringo SAY_SPR_END_7'),
+(-1000433,'%s fades away after a long pause.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,2,0,0,'ringo EMOTE_RIN_END_8'),
+(-1000434,'Liladris has been waiting for me at Maestra\'s Post, so we should make haste, $N.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'kerlonian SAY_KER_START'),
+(-1000435,'%s looks very sleepy...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,2,0,0,'kerlonian EMOTE_KER_SLEEP_1'),
+(-1000436,'%s suddenly falls asleep', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,2,0,0,'kerlonian EMOTE_KER_SLEEP_2'),
+(-1000437,'%s begins to drift off...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,2,0,0,'kerlonian EMOTE_KER_SLEEP_3'),
+(-1000438,'This looks like the perfect place for a nap...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'kerlonian SAY_KER_SLEEP_1'),
+(-1000439,'Yaaaaawwwwwnnnn...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'kerlonian SAY_KER_SLEEP_2'),
+(-1000440,'Oh, I am so tired...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'kerlonian SAY_KER_SLEEP_3'),
+(-1000441,'You don\'t mind if I stop here for a moment, do you?', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'kerlonian SAY_KER_SLEEP_4'),
+(-1000442,'Be on the alert! The Blackwood furbolgs are numerous in the area...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'kerlonian SAY_KER_ALERT_1'),
+(-1000443,'It\'s quiet... Too quiet...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'kerlonian SAY_KER_ALERT_2'),
+(-1000444,'Oh, I can see Liladris from here... Tell her I\'m here, won\'t you?', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,0,0,0,'kerlonian SAY_KER_END'),
+(-1000445,'%s wakes up!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,0,2,0,0,'kerlonian EMOTE_KER_AWAKEN');
 
 -- -1 033 000 SHADOWFANG KEEP
 INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES
@@ -2076,3 +2102,101 @@ INSERT INTO `npc_text` (`ID`, `text0_0`, `text0_1`, `lang0`, `prob0`, `em0_0`, `
 UPDATE `npc_option` SET `id`=52,`action`=19 WHERE `id`=51 AND `action`=18 AND `option_text`='UNIT_NPC_FLAG_OUTDOORPVP';
 DELETE FROM `npc_option` WHERE `id`=51;
 INSERT INTO `npc_option` (`id`, `gossip_id`, `npcflag`, `icon`, `action`, `box_money`, `coded`, `option_text`, `box_text`) values('51','0','16','0','18','10000000','0','Purchase a Dual Talent Specialization.','Are you sure you wish to purchase a Dual Talent Specialization?');
+
+
+   
+DELETE FROM script_texts WHERE entry BETWEEN -1609286 AND -1609201;
+INSERT INTO `script_texts` (`entry`,`content_default`,`sound`,`type`,`language`,`emote`,`comment`) VALUES
+-- The Light of Dawn
+-- pre text
+   (-1609201, 'Soldiers of the Scourge, stand ready! You will soon be able to unleash your fury upon the Argent Dawn!',14677,1,0,0,'Highlord Darion Mograine'),
+   (-1609202, 'The sky weeps at the devastation of sister earth! Soon, tears of blood will rain down upon us!',14678,1,0,0,'Highlord Darion Mograine'),
+   (-1609203, 'Death knights of Acherus, the death march begins!',14681,1,0,0,'Highlord Darion Mograine'),
+-- intro
+   (-1609204, 'Soldiers of the Scourge, death knights of Acherus, minions of the darkness: hear the call of the Highlord!',14679,1,0,22,'Highlord Darion Mograine'),
+   (-1609205, 'RISE!',14680,1,0,15,'Highlord Darion Mograine'),
+   (-1609206, 'The skies turn red with the blood of the fallen! The Lich King watches over us, minions! Onward! Leave only ashes and misery in your destructive wake!',14682,1,0,25,'Highlord Darion Mograine'),
+-- During the fight
+   (-1609207, 'Scourge armies approach!',0,1,0,0,'Korfax, Champion of the Light'),
+   (-1609208, 'Stand fast, brothers and sisters! The Light will prevail!',14487,1,0,0,'Lord Maxwell Tyrosus'),
+   (-1609209, 'Kneel before the Highlord!',14683,0,0,0,'Highlord Darion Mograine'),
+   (-1609210, 'You stand no chance!',14684,0,0,0,'Highlord Darion Mograine'),
+   (-1609211, 'The Scourge will destroy this place!',14685,0,0,0,'Highlord Darion Mograine'),
+   (-1609212, 'Your life is forfeit.',14686,0,0,0,'Highlord Darion Mograine'),
+   (-1609213, 'Life is meaningless without suffering.',14687,0,0,0,'Highlord Darion Mograine'),
+   (-1609214, 'How much longer will your forces hold out?',14688,0,0,0,'Highlord Darion Mograine'),
+   (-1609215, 'The Argent Dawn is finished!"',14689,0,0,0,'Highlord Darion Mograine'),
+   (-1609216, 'Spare no one!',14690,0,0,0,'Highlord Darion Mograine'),
+   (-1609217, 'What is this?! My... I cannot strike...',14691,0,0,0,'Highlord Darion Mograine'),
+   (-1609218, 'Obey me, blade!',14692,1,0,0,'Highlord Darion Mograine'),
+   (-1609219, 'You will do as I command! I am in control here!',14693,0,0,0,'Highlord Darion Mograine'),
+   (-1609220, 'I can not... the blade fights me.',14694,0,0,0,'Highlord Darion Mograine'),
+   (-1609221, 'What is happening to me?',14695,0,0,0,'Highlord Darion Mograine'),
+   (-1609222, 'Power...wanes...',14696,0,0,0,'Highlord Darion Mograine'),
+   (-1609223, 'Ashbringer defies me...',14697,0,0,0,'Highlord Darion Mograine'),
+   (-1609224, 'Minions, come to my aid!',14698,0,0,0,'Highlord Darion Mograine'),
+-- After the fight
+   (-1609225, 'You cannot win, Darion!',14584,1,0,0,'Highlord Tirion Fordring'),
+   (-1609226, 'Bring them before the chapel!',14585,1,0,0,'Highlord Tirion Fordring'),
+   (-1609227, 'Stand down, death knights. We have lost... The Light... This place... No hope...',14699,0,0,68,'Highlord Darion Mograine'),
+   (-1609228, 'Have you learned nothing, boy? You have become all that your father fought against! Like that coward, Arthas, you allowed yourself to be consumed by the darkness...the hate... Feeding upon the misery of those you tortured and killed!',14586,0,0,378,'Highlord Tirion Fordring'),
+   (-1609229, 'Your master knows what lies beneath the chapel. It is why he dares not show his face! He\'s sent you and your death knights to meet their doom, Darion.',14587,0,0,25,'Highlord Tirion Fordring'),
+   (-1609230, 'What you are feeling right now is the anguish of a thousand lost souls! Souls that you and your master brought here! The Light will tear you apart, Darion!',14588,0,0,1,'Highlord Tirion Fordring'),
+   (-1609231, 'Save your breath, old man. It might be the last you ever draw.',14700,0,0,25,'Highlord Darion Mograine'),
+   (-1609232, 'My son! My dear, beautiful boy!',14493,0,0,0,'Highlord Alexandros Mograine'),
+   (-1609233, 'Father!',14701,0,0,5,'Highlord Darion Mograine'),
+   (-1609234, 'Argh...what...is...',14702,0,0,68,'Highlord Darion Mograine'),
+   (-1609235, 'Father, you have returned!',14703,0,0,0,'Darion Mograine'),
+   (-1609236, 'You have been gone a long time, father. I thought...',14704,0,0,0,'Darion Mograine'),
+   (-1609237, 'Nothing could have kept me away from here, Darion. Not from my home and family.',14494,0,0,1,'Highlord Alexandros Mograine'),
+   (-1609238, 'Father, I wish to join you in the war against the undead. I want to fight! I can sit idle no longer!',14705,0,0,6,'Darion Mograine'),
+   (-1609239, 'Darion Mograine, you are barely of age to hold a sword, let alone battle the undead hordes of Lordaeron! I couldn\'t bear losing you. Even the thought...',14495,0,0,1,'Highlord Alexandros Mograine'),
+   (-1609240, 'If I die, father, I would rather it be on my feet, standing in defiance against the undead legions! If I die, father, I die with you!',14706,0,0,6,'Darion Mograine'),
+   (-1609241, 'My son, there will come a day when you will command the Ashbringer and, with it, mete justice across this land. I have no doubt that when that day finally comes, you will bring pride to our people and that Lordaeron will be a better place because of you. But, my son, that day is not today.',14496,0,0,1,'Highlord Alexandros Mograine'),
+   (-1609242, 'Do not forget...',14497,0,0,6,'Highlord Alexandros Mograine'),
+   (-1609243, 'Touching...',14803,1,0,0,'The Lich King'),
+   (-1609244, 'You have\'ve betrayed me! You betrayed us all you monster! Face the might of Mograine!',14707,1,0,0,'Highlord Darion Mograine'),
+   (-1609245, 'He\'s mine now...',14805,0,0,0,'The Lich King'),
+   (-1609246, 'Pathetic...',14804,0,0,0,'The Lich King'),
+   (-1609247, 'You\'re a damned monster, Arthas!',14589,0,0,25,'Highlord Tirion Fordring'),
+   (-1609248, 'You were right, Fordring. I did send them in to die. Their lives are meaningless, but yours...',14806,0,0,1,'The Lich King'),
+   (-1609249, 'How simple it was to draw the great Tirion Fordring out of hiding. You\'ve left yourself exposed, paladin. Nothing will save you...',14807,0,0,1,'The Lich King'),
+   (-1609250, 'ATTACK!!!',14488,1,0,0,'Lord Maxwell Tyrosus'),
+   (-1609251, 'APOCALYPSE!',14808,1,0,0,'The Lich King'),
+   (-1609252, 'That day is not today...',14708,0,0,0,'Highlord Darion Mograine'),
+   (-1609253, 'Tirion!',14709,1,0,0,'Highlord Darion Mograine'),
+   (-1609254, 'ARTHAS!!!!',14591,1,0,0,'Highlord Tirion Fordring'),
+   (-1609255, 'What is this?',14809,1,0,0,'The Lich King'),
+   (-1609256, 'Your end.',14592,1,0,0,'Highlord Tirion Fordring'),
+   (-1609257, 'Impossible...',14810,1,0,0,'The Lich King'),
+   (-1609258, 'This... isn\'t... over...',14811,1,0,25,'The Lich King'),
+   (-1609259, 'When next we meet it won\'t be on holy ground, paladin.',14812,1,0,1,'The Lich King'),
+   (-1609260, 'Rise, Darion, and listen...',14593,0,0,0,'Highlord Tirion Fordring'),
+   (-1609261, 'We have all been witness to a terrible tragedy. The blood of good men has been shed upon this soil! Honorable knights, slain defending their lives - our lives!',14594,0,0,0,'Highlord Tirion Fordring'),
+   (-1609262, 'And while such things can never be forgotten, we must remain vigilant in our cause!',14595,0,0,0,'Highlord Tirion Fordring'),
+   (-1609263, 'The Lich King must answer for what he has done and must not be allowed to cause further destruction to our world.',14596,0,0,0,'Highlord Tirion Fordring'),
+   (-1609264, 'I make a promise to you now, brothers and sisters: The Lich King will be defeated! On this day, I call for a union.',14597,0,0,0,'Highlord Tirion Fordring'),
+   (-1609265, 'The Argent Dawn and the Order of the Silver Hand will come together as one! We will succeed where so many before us have failed!',14598,0,0,0,'Highlord Tirion Fordring'),
+   (-1609266, 'We will take the fight to Arthas and tear down the walls of Icecrown!',14599,0,0,15,'Highlord Tirion Fordring'),
+   (-1609267, 'The Argent Crusade comes for you, Arthas!',14600,1,0,15,'Highlord Tirion Fordring'),
+   (-1609268, 'So too do the Knights of the Ebon Blade... While our kind has no place in your world, we will fight to bring an end to the Lich King. This I vow!',14710,0,0,1,'Highlord Darion Mograine'),
+-- Emotes
+   (-1609269, 'Thousands of Scourge rise up at the Highlord\'s command.',0,2,0,0,''),
+   (-1609270, 'The army marches towards Light\'s Hope Chapel.',0,2,0,0,''),
+   (-1609271, 'After over a hundred Defenders of the Light fall, Highlord Tirion Fordring arrives.',0,2,0,0,''),
+   (-1609272, 'flee',0,2,0,0,'Orbaz'),
+   (-1609273, 'kneels in defeat before Tirion Fordring.',0,2,0,0,'Highlord Darion Mograine'),
+   (-1609274, 'arrives.',0,2,0,0,'Highlord Alexandros Mograine'),
+   (-1609275, 'becomes a shade of his past, and walks up to his father.',0,2,0,0,'Highlord Darion Mograine'),
+   (-1609276, 'hugs his father.',0,2,0,0,'Darion Mograine'),
+   (-1609277, 'disappears, and the Lich King appears.',0,2,0,0,'Alexandros'),
+   (-1609278, 'becomes himself again...and is now angry.',0,2,0,0,'Highlord Darion Mograine'),
+   (-1609279, 'casts a spell on Tirion.',0,2,0,0,'The Lich King'),
+   (-1609280, 'gasps for air.',0,2,0,0,'Highlord Tirion Fordring'),
+   (-1609281, 'casts a powerful spell, killing the Defenders and knocking back the others.',0,2,0,0,'The Lich King'),
+   (-1609282, 'throws the Corrupted Ashbringer to Tirion, who catches it. Tirion becomes awash with Light, and the Ashbringer is cleansed.',0,2,0,0,'Highlord Darion Mograine'),
+   (-1609283, 'collapses.',0,2,0,0,'Highlord Darion Mograine'),
+   (-1609284, 'charges towards the Lich King, Ashbringer in hand and strikes the Lich King.',0,2,0,0,'Highlord Tirion Fordring'),
+   (-1609285, 'disappears. Tirion walks over to where Darion lay',0,2,0,0,'The Lich King'),
+   (-1609286, 'Light washes over the chapel  the Light of Dawn is uncovered.',0,2,0,0,'');
+
