8435ea7d5551f105316c72eb3bd45dfd979017d2
pfchrono <pfchrono@gmail.com>
2009-08-20 07:06:11 -0500

    [RS112] Merge TC2 changes rev5225 [TC] * Open 2 doors after Ingvar the Plunderer encounter.
    Signed-off-by: pfchrono <pfchrono@gmail.com>

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,9 +1,9 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
-#define _REVISION "bbd57b90b87538cdd00bee22ad8d18ca38f79ac6"
+#define _REVISION "4cff9e72a1cd7013fb8ea9dfd9c9d4ef60c51ceb"
 #define _HASH "*"
 #define _REVISION_DATE "2009-08-20"
-#define _REVISION_TIME "03:11:44"
+#define _REVISION_TIME "06:50:33"
 #define BUILD_USER "Midnight"
 #define BUILD_WORKSTATION "CHRONOTOSIS"
 #endif // __REVISION_H__
