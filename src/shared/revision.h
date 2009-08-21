9dd62d384d18a19d1eb7b6eb2d45ee4bc9d4a707
pfchrono <pfchrono@gmail.com>
2009-08-21 11:16:38 -0500

    [+] Removing Ace_Wrappers from solution and commiting prebuilt lib and dll, cuts down on recompiling this every time Signed-off-by: pfchrono <pfchrono@gmail.com>

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,9 +1,9 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
-#define _REVISION "4cff9e72a1cd7013fb8ea9dfd9c9d4ef60c51ceb"
+#define _REVISION "8435ea7d5551f105316c72eb3bd45dfd979017d2"
 #define _HASH "*"
 #define _REVISION_DATE "2009-08-20"
-#define _REVISION_TIME "06:50:33"
+#define _REVISION_TIME "07:06:11"
 #define BUILD_USER "Midnight"
 #define BUILD_WORKSTATION "CHRONOTOSIS"
 #endif // __REVISION_H__
