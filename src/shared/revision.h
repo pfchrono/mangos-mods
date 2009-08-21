51c5d1d244d17cb0a47a40a01077f0f6c2ad54c5
pfchrono <pfchrono@gmail.com>
2009-08-21 11:45:31 -0500

    [RV] Revert "[+] Removing Ace_Wrappers from solution and committing precompiled lib and dlls to help reduce total compile time"
    This reverts commit 03945b67bbe9e279bd376f668ebb95e585497a6c.

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,9 +1,9 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
-#define _REVISION "92515bf608a00ed2f0ac566ed8593ff780bddfee"
+#define _REVISION "4cff9e72a1cd7013fb8ea9dfd9c9d4ef60c51ceb"
 #define _HASH "*"
-#define _REVISION_DATE "2009-08-21"
-#define _REVISION_TIME "07:34:49"
+#define _REVISION_DATE "2009-08-20"
+#define _REVISION_TIME "06:50:33"
 #define BUILD_USER "Midnight"
 #define BUILD_WORKSTATION "CHRONOTOSIS"
 #endif // __REVISION_H__
